bd39c349863efe615861a19f7c4fd72972a2df82
bezoerb <ben@sommerlaune.com>
2019-01-17 23:27:39 +0100

    tweaked xo settings

--- index.js
+++ index.js
@@ -7,25 +7,30 @@ const debug = require('debug')('asset-resolver');
 const Bluebird = require('bluebird');
 const resolver = require('./lib/resolver');
 
-module.exports.getResource = function (file, opts) {
-	opts = defaults(opts || {}, {
-		base: [process.cwd()],
-		filter() {
-			return true;
-		}
-	});
+module.exports.getResource = function(file, opts) {
+  opts = defaults(opts || {}, {
+    base: [process.cwd()],
+    filter() {
+      return true;
+    }
+  });
 
-	if (typeof opts.base === 'string') {
-		opts.base = [opts.base];
-	}
+  if (typeof opts.base === 'string') {
+    opts.base = [opts.base];
+  }
 
-	opts.base = resolver.glob(toarray(opts.base));
+  opts.base = resolver.glob(toarray(opts.base));
 
-	return Bluebird.any(map(opts.base, base => { // eslint-disable-line promise/valid-params
-		return resolver.getResource(base, file, opts);
-	})).catch(Bluebird.AggregateError, errs => {
-		const msg = ['The file "' + file + '" could not be resolved because of:'].concat(map(errs, 'message'));
-		debug(msg);
-		return Bluebird.reject(new Error(msg.join(os.EOL)));
-	});
+  return Bluebird.any(
+    map(opts.base, base => {
+      // eslint-disable-line promise/valid-params
+      return resolver.getResource(base, file, opts);
+    })
+  ).catch(Bluebird.AggregateError, errs => {
+    const msg = [
+      'The file "' + file + '" could not be resolved because of:'
+    ].concat(map(errs, 'message'));
+    debug(msg);
+    return Bluebird.reject(new Error(msg.join(os.EOL)));
+  });
 };
--- test/test.js
+++ test/test.js
@@ -12,76 +12,91 @@ const readFile = Bluebird.promisify(fs.readFile);
 
 // Start fresh server before tests
 test.beforeEach(async t => {
-	const base = path.join(__dirname, 'fixtures');
-	const serve = serveStatic(base);
-	const port = await getPort();
-	const server = http.createServer((req, res) => serve(req, res, finalhandler(req, res)));
+  const base = path.join(__dirname, 'fixtures');
+  const serve = serveStatic(base);
+  const port = await getPort();
+  const server = http.createServer((req, res) =>
+    serve(req, res, finalhandler(req, res))
+  );
 
-	server.listen(port);
+  server.listen(port);
 
-	t.context.port = port;
-	t.context.server = server;
+  t.context.port = port;
+  t.context.server = server;
 });
 
 // Stop server in any case
 test.afterEach.cb.always(t => {
-	t.context.server.close(t.end);
+  t.context.server.close(t.end);
 });
 
 test('should fail on wrong url', async t => {
-	const res = resolver.getResource('blank.gif', {base: ['//localhost/']});
-	await t.throws(res, Error, /.*blank\.gif.*/);
+  const res = resolver.getResource('blank.gif', {base: ['//localhost/']});
+  await t.throws(res, Error, /.*blank\.gif.*/);
 });
 
 test('should find the file by url', async t => {
-	const expected = await readFile(path.join(__dirname, 'fixtures/blank.gif'));
-	const res = await resolver.getResource('blank.gif', {base: ['//localhost:' + t.context.port + '/', 'fixture']});
+  const expected = await readFile(path.join(__dirname, 'fixtures/blank.gif'));
+  const res = await resolver.getResource('blank.gif', {
+    base: ['//localhost:' + t.context.port + '/', 'fixture']
+  });
 
-	t.is(res.contents.toString(), expected.toString());
-	t.is(res.path, 'http://localhost:' + t.context.port + '/blank.gif');
-	t.is(res.mime, 'image/gif');
+  t.is(res.contents.toString(), expected.toString());
+  t.is(res.path, 'http://localhost:' + t.context.port + '/blank.gif');
+  t.is(res.mime, 'image/gif');
 });
 
 test('should find the file by file', async t => {
-	const expected = await readFile(path.join(__dirname, 'fixtures/check.svg'));
-	const res = await resolver.getResource('check.svg', {base: ['//localhost:' + t.context.port + '/test/', 'test/fixtures']});
+  const expected = await readFile(path.join(__dirname, 'fixtures/check.svg'));
+  const res = await resolver.getResource('check.svg', {
+    base: ['//localhost:' + t.context.port + '/test/', 'test/fixtures']
+  });
 
-	t.is(res.contents.toString(), expected.toString());
-	t.is(res.path, path.join('test', 'fixtures', 'check.svg'));
-	t.is(res.mime, 'image/svg+xml');
+  t.is(res.contents.toString(), expected.toString());
+  t.is(res.path, path.join('test', 'fixtures', 'check.svg'));
+  t.is(res.mime, 'image/svg+xml');
 });
 
 test('should find the file by glob', async t => {
-	const expected = await readFile(path.join(__dirname, 'fixtures/check.svg'));
-	const res = await resolver.getResource('check.svg', {base: [path.join(__dirname, '/*/')]});
+  const expected = await readFile(path.join(__dirname, 'fixtures/check.svg'));
+  const res = await resolver.getResource('check.svg', {
+    base: [path.join(__dirname, '/*/')]
+  });
 
-	t.is(res.contents.toString(), expected.toString());
-	t.is(res.path, path.join(__dirname, 'fixtures', 'check.svg'));
-	t.is(res.mime, 'image/svg+xml');
+  t.is(res.contents.toString(), expected.toString());
+  t.is(res.path, path.join(__dirname, 'fixtures', 'check.svg'));
+  t.is(res.mime, 'image/svg+xml');
 });
 
 test('should use consider sync filter', async t => {
-	const base = ['//localhost:' + t.context.port + '/'];
-	const filter = resource => {
-		return resource.path !== 'http://localhost:' + t.context.port + '/blank.gif' || resource.mime !== 'image/gif';
-	};
+  const base = ['//localhost:' + t.context.port + '/'];
+  const filter = resource => {
+    return (
+      resource.path !== 'http://localhost:' + t.context.port + '/blank.gif' ||
+      resource.mime !== 'image/gif'
+    );
+  };
 
-	const res = resolver.getResource('blank.gif', {base, filter});
+  const res = resolver.getResource('blank.gif', {base, filter});
 
-	await t.throws(res, Error, /blank\.gif.*could not be resolved.*rejected by filter/);
+  await t.throws(
+    res,
+    Error,
+    /blank\.gif.*could not be resolved.*rejected by filter/
+  );
 });
 
 test('should use consider async filter returning a promise', async t => {
-	const base = ['//localhost/', path.join(__dirname, 'fixtures')];
-	const filter = () => {
-		return new Bluebird((resolve, reject) => {
-			setTimeout(() => {
-				reject(new Error('FINE'));
-			}, 1000);
-		});
-	};
+  const base = ['//localhost/', path.join(__dirname, 'fixtures')];
+  const filter = () => {
+    return new Bluebird((resolve, reject) => {
+      setTimeout(() => {
+        reject(new Error('FINE'));
+      }, 1000);
+    });
+  };
 
-	const res = resolver.getResource('check.svg', {base, filter});
+  const res = resolver.getResource('check.svg', {base, filter});
 
-	await t.throws(res, Error, /check\.svg.*could not be resolved.*FINE/);
+  await t.throws(res, Error, /check\.svg.*could not be resolved.*FINE/);
 });
