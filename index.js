61502b42bdea19339f304308015a3cb3b8b8134e
Ben Zörb <benjamin.zoerb@jvm.de>
2020-05-15 00:32:39 +0200

    Breaking: Bump dependencies & require node 10

--- index.js
+++ index.js
@@ -1,48 +1,46 @@
-'use strict';
+"use strict";
 
-const debug = require('debug')('asset-resolver');
-const resolver = require('./lib/resolver');
+const debug = require("debug")("asset-resolver");
+const resolver = require("./lib/resolver");
 
 function any(promises) {
-  return Promise.all(
-    promises.map(promise =>
-      promise.then(
-        val => {
-          throw val;
-        },
-        reason => reason
-      )
-    )
-  ).then(
-    reasons => {
-      throw reasons;
-    },
-    firstResolved => firstResolved
-  );
+	return Promise.all(
+		promises.map((promise) =>
+			promise.then(
+				(val) => {
+					throw val;
+				},
+				(reason) => reason
+			)
+		)
+	).then(
+		(reasons) => {
+			throw reasons;
+		},
+		(firstResolved) => firstResolved
+	);
 }
 
 module.exports.getResource = (file, options = {}) => {
-  const opts = {
-    base: [process.cwd()],
-    filter: () => true,
-    ...options
-  };
+	const opts = {
+		base: [process.cwd()],
+		filter: () => true,
+		...options,
+	};
 
-  if (!Array.isArray(opts.base)) {
-    opts.base = [opts.base];
-  }
+	if (!Array.isArray(opts.base)) {
+		opts.base = [opts.base];
+	}
+	opts.base = resolver.glob([...opts.base]);
 
-  opts.base = resolver.glob([...opts.base]);
-
-  const promises = (opts.base || []).map(base => {
-    return resolver.getResource(base, file, opts);
-  });
-
-  return any(promises).catch(error => {
-    const msg = [`The file "${file}" could not be resolved because of:`].concat(
-      error.map(err => err.message)
-    );
-    debug(msg);
-    return Promise.reject(new Error(msg.join('\n')));
-  });
+	const promises = (opts.base || []).map((base) => {
+		return resolver.getResource(base, file, opts);
+	});
+	return any(promises).catch((error) => {
+		const msg = [`The file "${file}" could not be resolved because of:`].concat(
+			error.map((err) => err.message)
+		);
+		debug(msg);
+		return Promise.reject(new Error(msg.join("\n")));
+	});
 };
