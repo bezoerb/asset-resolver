615a7d51c5cad6dea054d47a38cd3f116c9241d8
Ben Zörb <benjamin.zoerb@jvm.de>
2020-05-18 01:39:29 +0200

    Fixed cli

--- cli.js
+++ cli.js
@@ -1,35 +1,40 @@
 #!/usr/bin/env node
 
-'use strict';
+"use strict";
 
-const meow = require('meow');
-const resolver = require('.');
+const meow = require("meow");
+const resolver = require(".");
 
 const cli = meow(
-  {
-    help: [
-      'Usage',
-      'asset-resolver [input]',
-      '',
-      'Options',
-      '  -b --base  List of directories/URLs where we should start looking for assets. [Default: process.cwd()]',
-      '',
-      'Examples',
-      "$ asset-resolver 'my.svg' -b 'some/directory' -b 'http://some.domain/assets'",
-      '<?xml version="1.0" encoding="UTF-8" standalone="no"?>',
-      '<svg>',
-      '...',
-      '</svg>'
-    ]
-  },
-  {alias: {b: 'base'}}
+	`
+Usage
+  $ asset-resolver <input>
+
+Options
+  -b --base  List of directories/URLs where we should start looking for assets. [Default: process.cwd()]
+
+Examples
+  asset-resolver 'my.svg' -b 'some/directory' -b 'http://some.domain/assets'
+  <?xml version="1.0" encoding="UTF-8" standalone="no"?>
+  <svg>
+  ...
+  </svg>
+`,
+	{
+		flags: {
+			base: {
+				type: "string",
+				alias: "b",
+			},
+		},
+	}
 );
 
 resolver
-  .getResource(cli.input[0], cli.flags)
-  .then((resource) => {
-    console.log(resource.contents);
-  })
-  .catch((error) => {
-    console.error(error.message || error);
-  });
+	.getResource(cli.input[0], cli.flags)
+	.then((resource) => {
+		console.log(resource.contents.toString());
+	})
+	.catch((error) => {
+		console.error(error.message || error);
+	});
